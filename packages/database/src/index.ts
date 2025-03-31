9a70c2454522741814a29a00890f16a0ab7604bc
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2025-03-31 14:37:07 +0200

    bad import

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,9 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { users } from "./schema";
+import { messages, experiences } from "./schema";
 
 const client = postgres(process.env.DATABASE_URL!);
-export const db = drizzle(client, { schema: { users } });
+export const db = drizzle(client, { schema: { messages, experiences } });
 
 export * from "./schema";
 export * from "./queries";
